0adb321617e32ab3be80f812252e636f160a2daa
Diego Gimeno <di15e36go@gmail.com>
2016-09-30 18:29:04 +0200

    probando ej.01

--- ejercicio01.php
+++ ejercicio01.php
@@ -1,1 +1,13 @@
-TODO:
+<!DOCTYPE html>
+<html><head>
+        <meta charset="UTF-8">
+        <title>ejemplo 5</title>
+</head><body>
+<?php
+//inicializamos
+echo "Array ordenado inicializado elemento por elemento <hr>";
+$array1[0]="elemento 1 ";
+$array1[1]="elemento 2 ";
+$array1[2]="elemento 3 ";
+$array1[3]="elemento 4 ";
+$array1[4]="elemento 5 ";
