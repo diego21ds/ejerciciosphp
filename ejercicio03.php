3acd8a4857743db03d97256f2a34dfe4055987e8
Diego Gimeno <di15e36go@gmail.com>
2016-10-03 18:24:44 +0200

    probando ej.03

---
+++ ejercicio03.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html><head>
+        <meta charset="UTF-8">
+        <title>ejemplo 5</title>
+</head><body>
+<?php
+
+/*Ejercicio03.php. Crea una array asociativo con las cinco posiciones. En cada posición debes almacenar uno o más jugadores en un array ordenado. Muestra toda la plantilla usando listas no ordenadas html (ul y li) */
+
+//inicializamos
+echo '<h2>Arrays asociativos</h2>';
+
+
+$array1 = array (
+    'base' => array('Pedro','Carlos'),
+    'escolta' => array('Manuel','Javier'),
+    'alero' => array('Daniel','Ruben'),
+    'alapivot' => array('Alberto','Rafa'),
+    'pivot' => array('Leandro','Andrés'),	
+); 
+
+
+
+//foreach clave->elemento
+echo "Recorrido mediante bucle foreach con posicion->elemento <br>";
+foreach ($array1 as $key => $nombres){
+	echo "Posición $key";
+	foreach ($nombres as $nombre){
+    echo  "<li>" . $nombre . '</li><br>';
+	}
+}
+
+var_dump($array1);
+?> 
+
+
+</body></html>
